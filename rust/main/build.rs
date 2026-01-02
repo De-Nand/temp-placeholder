08e4aa7f342985e06ba0ace55aa9058355409733
De-Nand <61670552+De-Nand@users.noreply.github.com>
2026-01-02 13:27:40 -0500

    Rust & minimal SDL integration?

--- rust/main/build.rs
+++ rust/main/build.rs
@@ -1,17 +1,51 @@
+use std::{
+    env, fs,
+    io::{BufWriter, Write},
+    path::{self, PathBuf},
+};
+
 fn main() {
     println!("Starting the build.rs");
 
-    let headers = "../../sdls/windows/include";
-    let sdl_library = "../../sdls/windows/lib";
+    println!("cargo:rustc-link-arg=-v");
+    //let home = env::var("HOME").expect("Home environment variable should be set");
+    // Cannot use the ~ to find the location of SDL as it will not be expanded by the linker.
+    // Have to format the absolute path to get the location
+    //let framework_path = format!("{}/SDL", home);
+    let framework_path = "../../sdls/macos/SDL";
+    //let framework_path_core = format!("{}/SDL3.framework/SDL3", framework_path);
+    //let framework_headers_core = "./SDL3";
+    let framework_headers_core = format!("{}/SDL3.framework/Headers", framework_path);
+    //let framework_headers_image = format!("{}/SDL3_image.framework/Headers", framework_path);
+    //let framework_headers_ttf = format!("{}/SDL3_ttf.framework/Headers", framework_path);
+    println!("cargo:rustc-link-search=framework={}", &framework_path);
+    println!("cargo:rustc-link-lib=framework=SDL3");
 
-    println!("cargo:rustc-link-arg=I{}", headers);
-    println!("cargo:rustc-link-arg=L{}", sdl_library);
-    println!("cargo:rustc-link-lib=dylib=SDL3");
+    //create_wrapper_file(&framework_headers_core);
 
-    //pkg_config::Config::new().probe("SDL3").unwrap();
+    let out_path = PathBuf::from(env::var("OUT_DIR").expect("Output directory to be available"));
+    //    .clang_arg(format!("-rpath {}", &framework_path))
 
-    //cc::Build::new()
-    //    .include("../../../sdls/include")
-    //    .compile("main");
+    bindgen::Builder::default()
+        .header("sdl_wrapper2.h")
+        .clang_arg(format!("-I{}", &framework_headers_core))
+        .clang_arg(format!("-F{}", &framework_path))
+        .parse_callbacks(Box::new(bindgen::CargoCallbacks::new()))
+        .generate()
+        .expect("Bindings to be setup")
+        .write_to_file(out_path.join("bindings.rs"))
+        .expect("could not write bindings");
 
+    println!("cargo:rustc-link-arg=-Wl,-rpath,{}", &framework_path);
+}
+
+fn create_wrapper_file(framework_headers_core: &String) {
+    let sdl_wrapper = fs::File::create("sdl_wrapper.h").unwrap();
+    let mut writer = BufWriter::new(sdl_wrapper);
+
+    for header_file in fs::read_dir(framework_headers_core).expect("SDL headers available") {
+        let hf = header_file.unwrap().file_name().into_string().unwrap();
+        _ = writeln!(&mut writer, "#include \"{}\"", hf);
+    }
+    _ = writer.flush();
 }
--- rust/main/src/main.rs
+++ rust/main/src/main.rs
@@ -1,10 +1,31 @@
-#[link(name = "SDL3")]
-unsafe extern "C" {
-    fn SDL_INIT(x: u32) -> bool;
-}
+#![allow(non_upper_case_globals)]
+#![allow(non_camel_case_types)]
+#![allow(non_snake_case)]
+
+use std::{
+    env,
+    ffi::CString,
+    os::raw::{c_char, c_int},
+};
+include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
 
 fn main() {
-    //unsafe {SDL_INIT(0x00000020);}
-    unsafe {SDL_INIT(0);}
+    println!("Hello, world!, {}", env!("OUT_DIR"));
+    let mut running: bool = true;
+    let mut count: u32 = 0;
+    unsafe {
+        SDL_Init(SDL_INIT_VIDEO);
+        const width: c_int = 1000;
+        const height: c_int = 500;
+        let string_title = CString::new("SDL3 Rust").unwrap();
+        let title: *const c_char = string_title.as_ptr();
+        let window: *mut SDL_Window = SDL_CreateWindow(title, width, height, 0);
+        const temp: *const c_char = std::ptr::null();
+        let renderer: *mut SDL_Renderer = SDL_CreateRenderer(window, temp);
+
+        while running == true {
+            count = count + 1;
+        }
+    }
     println!("Hello, world!");
 }
